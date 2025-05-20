a9120ee114acfbf7a981b5b8a576f3584900bcef
Mikkel Johan Meldgaard Raben <mikkel@meldgaard.net>
2025-05-20 15:18:06 +0200

    settings 2

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ extern crate rocket;
 
 use core::str;
 
+use bson::{oid::ObjectId, Bson};
 use rocket::{
     serde::{self, json::Json, Deserialize},
     State,
 };
 
+use ::serde::Serialize;
 use mongodb::{
     bson::{doc, Document},
     Client, Collection,
@@ -23,41 +25,55 @@ struct Thing<'r> {
     measurement: bool,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct UserSettings {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    timeLimit: i32,
+    colorWalk: [i32; 3],
+    colorAlarm: [i32; 3],
+    startTime: String,
+    endTime: String,
+}
+
 struct Collections {
     users: Collection<Document>,
     measurements: Collection<Document>,
 }
 
-
 #[post("/", data = "<thing>")]
 async fn post_index(repo: &State<Collections>, thing: Json<Thing<'_>>) -> () {
-    repo.measurements.insert_one(
-        doc! {
-            "device_id": thing.device_id,
-            "device_type": thing.device_type,
-            "measurement": thing.measurement,
-            "measure_time": chrono::Utc::now().to_rfc3339()
-        },
-        None,
-    )
-    .await
-    .expect("Failed to insert document");
+    repo.measurements
+        .insert_one(
+            doc! {
+                "device_id": thing.device_id,
+                "device_type": thing.device_type,
+                "measurement": thing.measurement,
+                "measure_time": chrono::Utc::now().to_rfc3339()
+            },
+            None,
+        )
+        .await
+        .expect("Failed to insert document");
 }
 
 #[get("/settings", data = "<user>")]
 async fn get_settings(repo: &State<Collections>, user: &str) -> String {
     let filter = doc! { "user": user };
-    let result: Option<Document> = repo.users.find_one(filter, None).await.unwrap();
-    match result {
-        Some(doc) => {
-            let json = serde::json::to_string(&doc).unwrap();
+    let result = repo.users.find_one(Some(filter), None).await.unwrap();
+    let docu = result.unwrap();
+
+    let user_settings: Result<UserSettings, bson::de::Error> =
+        bson::from_bson(bson::Bson::Document(docu));
+    match user_settings {
+        Ok(settings) => {
+            let json = serde::json::to_string(&settings).unwrap();
             return json;
         }
-        None => {
-            println!("No document found for user {}", user);
+        Err(_) => {
+            return String::new();
         }
     }
-    String::new()
 }
 
 #[launch]
